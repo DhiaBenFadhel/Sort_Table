d5526be34afc8137b9b0dd95634b22bf5770c9e8
DhiaBenFadhel <dhiabenfadhel@gmail.com>
2022-01-05 20:28:48 +0100

    Here the table is filled with random integers and sorted from lower to greater by selection also

---
+++ TriRandom_Select.py
@@ -0,0 +1,39 @@
+from random import*
+def saisie():
+    n=int(input("Entrez la taille de tableau: "))
+    while not(n>1):
+        n=int(input("Entrez la taille de tableau: "))  
+    return n
+
+def remplir(t,n):
+    for i in range(n):
+        t[i] =randint(0,999)
+
+def affichage(t,n):
+    for i in range(n):
+        print(t[i],end=" ")
+
+def PremPosMin(t,n,d):
+    Pm=d
+    for i in range(d+1,n):
+        if t[i]<t[Pm]:
+            Pm=i
+    return Pm
+
+def TriSelect(t,n):
+    for i in range(n-1):
+        PPM=PremPosMin(t,n,i)
+        if PPM!=i:
+            x=t[i]
+            t[i]=t[PPM]
+            t[PPM]=x
+
+
+n=saisie()
+t=[0]*n
+remplir(t,n)
+TriSelect(t,n)
+print("Ici votre tableau trieé :")
+affichage(t,n)
+
+
